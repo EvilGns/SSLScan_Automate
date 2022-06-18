f8c9f340d7e923400066fd1bd5e3c783f914fcec
Mohamed Yoosuf Afendeen Mohamed Aamir <71857780+EvilGns@users.noreply.github.com>
2022-06-18 17:27:29 +0530

    Removed Unwanted Lines
    ( | sort -u | xargs | tr ' ' ' , ' ) Removed the following line from the script which deemed unwanted.

--- SSLScan_Script.sh
+++ SSLScan_Script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-cat *.nmap | grep "open" | grep -v "filtered" | cut -d "/" -f 1 | sort -u | xargs | tr ' ' ' , ' > ports.txt
+cat *.nmap | grep "open" | grep -v "filtered" | cut -d "/" -f 1  > ports.txt
 IP=$1
 filename=ports.txt
 while read line; do
